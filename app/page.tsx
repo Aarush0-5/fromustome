b1877342bfe959f71559e1d9713c1e905e93fd41
Aarush0-5 <singhaar0110@gmail.com>
2026-03-29 13:32:35 +0530

    stuff changed

--- app/page.tsx
+++ app/page.tsx
@@ -75,6 +75,20 @@ export default function Home() {
           </Link>
         </div>
       </section>
+        <section className="mt-5 mb-10">
+          <div className="mt-6 flex  flex-col items-center border-t border-slate-100 pt-6">
+              <span className="text-xs uppercase tracking-widest text-slate-400 mb-3">
+                Get in touch
+              </span>
+              <a 
+                href={`mailto:${process.env.NEXT_PUBLIC_MAIL}`}
+                className="px-6 py-2 bg-slate-900 text-white rounded-full text-sm font-medium hover:bg-slate-800 transition-all flex items-center gap-2"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="20" height="16" x="2" y="4" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg>
+                Email the Developer
+              </a>
+            </div>
+        </section>
     </main>
   )
 }
